33b55506ea560e4741c7e3d0318eca686a7d0f8e
vitorlpx <lopessccp.3k@gmail.com>
2026-03-26 22:34:03 -0300

    feat: adicionando main.py inicial

--- app/src/main.py
+++ app/src/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from src!")
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
